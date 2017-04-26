b9c63b9e9aae8c7b2f5f0964d08d8117983f2950
firesome <tanya.zanadvorova@gmail.com>
2017-04-26 20:56:56 +0300

    made a branch for the further development

--- src/main/java/io/khasang/restaurant/config/AppConfig.java
+++ src/main/java/io/khasang/restaurant/config/AppConfig.java
@@ -3,10 +3,12 @@ package io.khasang.restaurant.config;
 import io.khasang.restaurant.model.Message;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
     @Bean
+    @Scope("prototype")
     public Message message(){
         return new Message("hello bean!");
     }
