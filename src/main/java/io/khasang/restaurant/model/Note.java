9a292182b648d1b166e1f0f5de2a14bd2a30f594
gmolchano <mol-galina@rambler.ru>
2017-05-08 16:12:30 +0300

    add Note bean

--- src/main/java/io/khasang/restaurant/model/Note.java
+++ src/main/java/io/khasang/restaurant/model/Note.java
@@ -1,7 +1,23 @@
 package io.khasang.restaurant.model;
 
-/**
- * Created by Galina on 08.05.2017.
- */
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Note {
+    private String text;
+
+    public Note() {
+    }
+
+    @Autowired
+    public Note(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
