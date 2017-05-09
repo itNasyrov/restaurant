ad93afec3cb3a51a5811f3e32c635b7bff029564
vladimir <vladimviv@mail.ru>
2017-05-09 10:00:06 +0300

    add hibernate (hibernateConfig.java, Company.java, hibernate.properties)

---
+++ src/main/java/io/khasang/restaurant/entity/Company.java
@@ -0,0 +1,34 @@
+package io.khasang.restaurant.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="companies")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="company_id")
+    private long id;
+
+    @Column(name="company_name")
+    private String name;
+
+    private int count;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
