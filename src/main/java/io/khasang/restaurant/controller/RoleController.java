3326e8c5bffce3a949302da843023b416cc96ae1
Dinar Khalfin <khdi1011@hs-karlsruhe.de>
2017-06-01 20:59:13 +0300

    updated PUT-method in USER and ROLE classes

--- src/main/java/io/khasang/restaurant/controller/RoleController.java
+++ src/main/java/io/khasang/restaurant/controller/RoleController.java
@@ -4,10 +4,9 @@ import io.khasang.restaurant.entity.Role;
 import io.khasang.restaurant.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/role")
@@ -15,12 +14,11 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
-    //@RequestMapping(value ="/add", method = RequestMethod.PUT, produces = "application/json;charset=utf-8");
-    @RequestMapping(value ="/add", method = RequestMethod.PUT, produces = "application/json;charset=utf-8");
+    @RequestMapping(value = "/add", method = RequestMethod.PUT, produces = "application/json;charset=utf-8")
     @ResponseBody
     public Role addRole(@RequestBody Role role){
+        
         return roleService.addRole(role);
-
     }
 
 
--- src/main/java/io/khasang/restaurant/controller/UserController.java
+++ src/main/java/io/khasang/restaurant/controller/UserController.java
@@ -1,13 +1,14 @@
 package io.khasang.restaurant.controller;
 
+
+import io.khasang.restaurant.entity.Document;
 import io.khasang.restaurant.entity.User;
 import io.khasang.restaurant.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -15,11 +16,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value ="/add", method = RequestMethod.PUT, produces = "application/json;charset=utf-8");
+    @RequestMapping(value = "/add", method = RequestMethod.PUT, produces = "application/json;charset=utf-8")
     @ResponseBody
     public User addUser(@RequestBody User user){
+
         return userService.addUser(user);
     }
 
-    @RequestMapping
 }
